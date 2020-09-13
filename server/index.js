67f18afbf54b945de831e344a65e3ae1fc38f5cb
kim <programmer97@naver.com>
2020-09-13 20:16:59 +0900

    server

--- server/index.js
+++ server/index.js
@@ -2,50 +2,20 @@ const express = require('express');
 const app = express();
 const logger = require('morgan');
 const bodyParser = require('body-parser');
+const { response } = require('express');
 
 const apiRouter = express.Router();
 
 app.use(logger('dev', {}));
 app.use(bodyParser.json());
-app.use('/api', apiRouter);
-//이 코드의 결과로, ‘/api/**’로 시작하는 모든 요청은 apiRouter에게 전달됩니다.
 
-apiRouter.post('/sayHello', function(req, res) {
+apiRouter.post('', (req, res) => {
     const responseBody = {
-        version: "2.0",
-        template: {
-            outputs: [
-                {
-                    simpleText: {
-                        text: "hello I'm hotplace"
-                    }
-                }
-            ]
-        }
-    };
+        result: 'hello world'
+    }
 
-    res.status(200).send(responseBody)
-});
-
-apiRouter.post('/showHello', function(req, res) {
-    console.log(req.body);
-
-    const responseBody = {
-        version: "2.0",
-        template: {
-            outputs: [
-                {
-                    simpleImage: {
-                        imageUrl: "https://t1.daumcdn.net/friends/prod/category/M001_friends_ryan2.jpg",
-                        altText: "hello I'm hotplace"
-                    }
-                }
-            ]
-        }
-    };
-
-    res.status(200).send(responseBody);
-});
+    res.send(responseBody)
+})
 
 app.listen(4000, function() {
     console.log('Example skill server listening on port 4000!');
